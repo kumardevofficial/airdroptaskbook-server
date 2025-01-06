0689e80a7dd3ec837bec8e406ed5873483b3dcb2
kumardevofficial <112534262+kumardevofficial@users.noreply.github.com>
2025-01-06 11:05:47 +0530

    update the project form

--- index.js
+++ index.js
@@ -6,17 +6,16 @@ import galxeRouter from './Routes/GalxeRoutes.js';
 import waitlistRouter from './Routes/WaitlistRoutes.js';
 import projectRouter       from './Routes/ProjectRoutes.js';
 import cors from 'cors';
-import multer from 'multer';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname, join } from 'path';
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 
 // Setup __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 // Middleware
 app.use(express.json());
@@ -28,19 +27,6 @@ app.use(
   })
 );
 
-// Serve static files
-app.use('/uploads', express.static(join(__dirname, 'uploads')));
-
-// Multer configuration
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
-const upload = multer({ storage });
 
 // Routes
 app.get('/', (req, res) => {
